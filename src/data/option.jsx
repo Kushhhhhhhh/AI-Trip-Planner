0331736abe3a94659be3c612ddbfa7448b165df5
kush sharma <kush.73888@gmail.com>
2024-08-02 01:55:48 +0530

    Other data

---
+++ src/data/option.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelList = [
+    {
+        id: 1,
+        title: "Just me",
+        desc: "A sole traveles in exploration",
+        icon: "✈️",
+        people: "1"
+    },
+    {
+        id: 2,
+        title: "A Couple",
+        desc: "Two traveles in tandem",
+        icon: "🥂",
+        people: "2 People"
+    },
+    {
+        id: 3,
+        title: "Family",
+        desc: "A group of fun loving adv",
+        icon: "🏡",
+        people: "3 to 5 People"
+    },
+    {
+        id: 4,
+        title: "Friends",
+        desc: "A bunch of thrill-seeks",
+        icon: "⛵",
+        people: "5 to 10 People"
+    }
+]
+
+export const SelectBudgetOptions = [
+    {
+        id: 1,
+        title: 'Cheap',
+        desc: 'Stay conscious of costs',
+        icon: '💵',
+    },
+    {
+        id: 2,
+        title: 'Moderate',
+        desc: 'Keep cost on the average side',
+        icon: '💰',
+    },
+    {
+        id: 3,
+        title: 'Luxury',
+        desc: 'No worries about cost',
+        icon: '💸',
+    }
+]
+
+export const AI_PROMPT='Generate Travel Plan for Location: {location} for {stay} Days for {travel} people with a {budget} budget, give me Hotels options list  with hotel name, hotel address, price , image url, geo coordinates, rating, descriptions, and suggest itinerary with place name , place details, place image url, geo coordinates, ticket pricing, rating, time travel for each location for {stay} days with each day plan with best time to visit in JSON Format'
